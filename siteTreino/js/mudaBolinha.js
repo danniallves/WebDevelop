1e83695c6a6cc04cd2bc065998a8b4676ae6bff7
Be <bernardoags@gmail.com>
2016-08-09 10:24:25 -0300

    modifiquei bolinha.js para só conter a função changeColor, um código muito melhor e mais enxuto.

--- siteTreino/js/mudaBolinha.js
+++ siteTreino/js/mudaBolinha.js
@@ -1,51 +1,20 @@
 
-var counter = 0;
-var cores = ["red", "green", "blue"];
-var bolinhas = document.getElementsByClassName("bolinha");
-
-
-function mudaCor(elem) {
-	"use strict";
-    counter++;
-	elem.style.backgroundColor = cores[counter % 3];
-
-}
-
 var counters = [];
 var elems = [];
+var cores = ["red", "green", "blue"];
 
-function contains(arr, ident) {
-    "use strict";
-    var i;
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i] === ident) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function addElement(elem) {
-    "use strict";
-    if (!(contains(elems, elem.id))) {
-        counters.push(0);
-        elems.push(elem.id);
-    }
-}
 
 function changeColor(elem) {
     "use strict";
-    var i;
-    addElement(elem);
-    i = elems.indexOf(elem.id);
+    var i = elems.indexOf(elem.id);
+    if (i === -1) {
+        counters.push(0);
+        elems.push(elem.id);
+        i = elems.indexOf(elem.id);
+    }
+    /*addElement(elem);
+    i = elems.indexOf(elem.id);*/
     counters[i]++;
     elem.style.backgroundColor = cores[counters[i] % 3];
 }
-
-
-
-
-
-
-
 
