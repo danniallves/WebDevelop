0c04f728d12e247906efc8477963bd762ac16182
Be <bernardoags@gmail.com>
2016-08-09 10:12:22 -0300

    mudei o onclick das bolinhas para a função changeColor e fiz essa função e outras que ela utiliza no javascript para que as bolinhas mudem de cor separadamente.

--- siteTreino/js/mudaBolinha.js
+++ siteTreino/js/mudaBolinha.js
@@ -3,10 +3,49 @@ var counter = 0;
 var cores = ["red", "green", "blue"];
 var bolinhas = document.getElementsByClassName("bolinha");
 
+
 function mudaCor(elem) {
 	"use strict";
     counter++;
 	elem.style.backgroundColor = cores[counter % 3];
 
 }
+
+var counters = [];
+var elems = [];
+
+function contains(arr, ident) {
+    "use strict";
+    var i;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] === ident) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function addElement(elem) {
+    "use strict";
+    if (!(contains(elems, elem.id))) {
+        counters.push(0);
+        elems.push(elem.id);
+    }
+}
+
+function changeColor(elem) {
+    "use strict";
+    var i;
+    addElement(elem);
+    i = elems.indexOf(elem.id);
+    counters[i]++;
+    elem.style.backgroundColor = cores[counters[i] % 3];
+}
+
+
+
+
+
+
+
 
